ceb07fdeb37af99164bf06edcaadc5d04c6b0d8a
KrisKashi <justkristenhaslam@gmail.com>
2026-08-12 16:28:09 +0100

    Completed Backup script

--- bash-scripting/my_scripts/BackupScript.sh
+++ bash-scripting/my_scripts/BackupScript.sh
@@ -16,10 +16,20 @@ Display count of files backed up
 
 '
 
+
+Backupfiles(){
+
 local File_Path
-count = 0
-echo " Please enter source directory for backup:"
-read File_Path
-mkdir "$File_path"-backup
-cp "$File_path"/*.txt "$File_path"-backup
-mv "$File_path"-backup backup-$(date +"%m-%d-%y-%r")
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+# Its important to set the variable for timestamp first so you can refer to it easier after
+
+echo "Please enter source directory for backup:"
+read File_Path      # Takes user input as File_Path
+DIR="$File_Path"-backup-"$timestamp"         # We set directory here so that it has file_path, earlier wouldve made it empty 
+mkdir "$DIR" 
+cp "$File_Path"/*.txt "$DIR"                #Copies all text files to backup directory
+echo "Backed up "$( find "$DIR" -type f | wc -l )" files" # Counts how many files backed up
+
+}
+
+Backupfiles
